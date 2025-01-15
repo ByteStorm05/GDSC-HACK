c7fd88f86bb9b72491c5096b12540f4ed22e4eab
ByteStorm05 <143643985+ByteStorm05@users.noreply.github.com>
2025-01-15 19:17:58 +0530

    Removed debbing points

--- src/pages/VoiceSearchPage.jsx
+++ src/pages/VoiceSearchPage.jsx
@@ -52,13 +52,7 @@ const ChatPage = () => {
     <div>
       <AmenityVoiceInput setAmenitiesList={setAmenitiesList} />
 
-      {/* Optionally, show the amenities list */}
-      {amenitiesList && (
-        <div style={{ marginTop: '20px' }}>
-          <strong>Selected Amenities:</strong>
-          <pre>{amenitiesList}</pre>
-        </div>
-      )}
+     
 
       <div className="max-w-7xl mx-auto p-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
